5ae3b3d3fc1aea45270c7b0dfd72785a6485f7e7
Marcelo Bauer Sabaraense <bauersaba@gmail.com>
2022-06-15 12:22:19 -0300

    Atualiza tudo

--- tfms/acionado/acionadoModel.java
+++ tfms/acionado/acionadoModel.java
@@ -1,3 +1,19 @@
+package tfms.acionado;
 public class acionadoModel{
-    
+    private int codigo;
+    private servico();
+    private boolean ativado;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public boolean isAtivado() {
+        return ativado;
+    }
+    public void setAtivado(boolean ativado) {
+        this.ativado = ativado;
+    }
 }
